fd45a4672a283bf92b061e83e57971d16e59cc35
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-07-04 11:37:11 +0300

    birthday-project

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import data from "./data";
+import "./App.css";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([])
+          }}
+        >
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
